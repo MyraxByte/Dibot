c65a6ca60dbaa8780afcf0bb78b65dadd99e93b4
Mirax <Dragonmorn@gmail.com>
2019-06-24 23:55:49 +0300

    Update Messages.js

--- discord/events/Messages.js
+++ discord/events/Messages.js
@@ -3,11 +3,9 @@ module.exports = client => {
         if (message.author.bot) return
         if (!message.channel.permissionsFor(message.guild.me).missing("SEND_MESSAGES")) return
     	
-        // Получаем аргументы и команды с сообщений
 		const args = message.content.trim().split(/ +/g)
 		const command = args.shift().slice(Dibot.config.prefix.length).toLowerCase()
 		
-		// Если мы не можем найти пользователя = находим
 		if (message.guild && !message.member) await message.guild.fetchMember(message.author)
 		
 
